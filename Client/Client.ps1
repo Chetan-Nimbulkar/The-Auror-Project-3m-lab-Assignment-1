0b912c8f248a642388f4584d0561df73280ba1a3
Chetan Nimbulkar <chetan.nimbulkar105@gmail.com>
2022-05-01 11:47:23 +0530

    IP modification and cleanup

--- Client/Client.ps1
+++ Client/Client.ps1
@@ -2,28 +2,22 @@
 # Turn firewall off
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
 
+$chromepath = $env:USERPROFILE + "\Downloads\GoogleChrome.exe"
+Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" `
+-OutFile $chromepath
+
+Start-Process -FilePath $chromepath -ArgumentList "/install /silent"
+
 # Set DNS address to client
-$adapters = Get-WmiObject Win32_NetworkAdapterConfiguration
-$adapters | ForEach-Object {$_.SetDNSServerSearchOrder("10.0.0.9")}
+Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses "10.0.0.9"
 # same IP from Server Vagrantfile
 
-$username = "vagrant"
-$Password = ConvertTo-SecureString -AsPlainText $"vagrant" -Force
+$username = "Vagrant"
+$Password = ConvertTo-SecureString -AsPlainText "vagrant" -Force
 
 $creds = New-Object System.Management.Automation.PSCredential $username, $Password
 
 Add-Computer -DomainName auror.local -credential $creds
 
 # here instead of making administrator account and adding to forest, we have to add the domain account to client machine
-net localgroup "Administrators" auror\adam /add
-
-$chromepath = $env:USERPROFILE + "\Downloads\GoogleChrome.exe"
-Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" `
--OutFile $chromepath
-
-Start-Process -FilePath $chromepath -ArgumentList "/quiet /install"
-
-
-
- 
-
+net localgroup "Administrators" auror\Adam /add
